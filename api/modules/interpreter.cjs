2e2fd80581f51dacbd7f663a859182d98fe04fb0
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 17:48:45 -0500

    style: js-beautify formatting (api/modules/interpreter.cjs)

--- api/modules/interpreter.cjs
+++ api/modules/interpreter.cjs
@@ -1,64 +1,64 @@
-const util = require('node:util'); 
+const util = require('node:util');
 require('core-js');
 
-
-
-globalThis.interpretCode = async function(code,options) {
-  code = code.replace(/%2F%2F%.*%0A/g,'');
-  let output ='';
+globalThis.interpretCode = async function(code, options) {
+  code = code.replace(/%2F%2F%.*%0A/g, '');
+  let output = '';
   let log = '';
 
+  if (!(console.backuplog)) {
+    console.backuplog = console.log;
+  }
+  console.log = function() {
+    for (let i = 0; i < arguments.length; i++) {
+      log += util.inspect(arguments[i]);
+    }
+  }
 
-    if(!(console.backuplog))
-    {console.backuplog=console.log;}
-    console.log = function(){for(let i=0;i<arguments.length;i++){log+=util.inspect(arguments[i]);}}
+  let strict = '';
 
-let strict = '';
-
-  if(options?.strict){
+  if (options?.strict) {
     strict = '"use ' + 'strict";';
   }
 
-
-
-    try{
-      if(options?.module){
-        try{
-       output = await import(`data:text/javascript,${strict}${new Date().getTime()};${decodeURIComponent(code)}`);
-        }catch(e){
-          log += e.message;
-          output = await import(`data:text/javascript;base64,${btoa(`${strict}${new Date().getTime()};${decodeURIComponent(code)}`)}`);
-        }
-      } else{ 
-        if(options?.async){
-        output = await eval(`${strict}${decodeURIComponent(code)}`);
-        }else{
-          output = eval(`${strict}${decodeURIComponent(code)}`);
-        }
+  try {
+    if (options?.module) {
+      try {
+        output = await import(`data:text/javascript,${strict}${new Date().getTime()};${decodeURIComponent(code)}`);
+      } catch (e) {
+        log += e.message;
+        output = await import(`data:text/javascript;base64,${btoa(`${strict}${new Date().getTime()};${decodeURIComponent(code)}`)}`);
       }
-    }catch(e){
-      log += e.message;
-      if(!output){
-        output = e;
+    } else {
+      if (options?.async) {
+        output = await eval(`${strict}${decodeURIComponent(code)}`);
+      } else {
+        output = eval(`${strict}${decodeURIComponent(code)}`);
       }
     }
-
-  console.log = console.backuplog;
-try{
-  if(`${output}`=='[object Object]'){
-    if((output.value)&&(!(`${output.value}`.startsWith('code=')))){
-      output=output.value;
-    }else{
-      output = util.inspect(output);
+  } catch (e) {
+    log += e.message;
+    if (!output) {
+      output = e;
     }
   }
-}catch(e){
-  try{output = util.inspect(output);}catch(e){}
+
+  console.log = console.backuplog;
+  try {
+    if (`${output}` == '[object Object]') {
+      if ((output.value) && (!(`${output.value}`.startsWith('code=')))) {
+        output = output.value;
+      } else {
+        output = util.inspect(output);
+      }
+    }
+  } catch (e) {
+    try {
+      output = util.inspect(output);
+    } catch (e) {}
     console.log(e.message);
-}
+  }
   output = `${output}`;
 
-  return 'return: '+output+'\nlog: '+log;
+  return 'return: ' + output + '\nlog: ' + log;
 }
-
-
